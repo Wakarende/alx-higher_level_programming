1986e85793957f315e3b0ff6be9c363879eebe34
Wakarende <joykirii@gmail.com>
2022-06-06 16:57:15 +0300

    function that retrieves an element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
-from ast import Return
-
-
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
-    elif idx >= len(my_list):
-        return None
-    else:
-        return my_list[idx]
+    return my_list[idx]
